eb98a7db15c5ffd07f8450001085ec24afad12e0
nobita54 <a.k639583027@gmail.com>
2025-11-23 02:04:48 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# -150" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/nobita54/-150.git
+git push -u origin main
