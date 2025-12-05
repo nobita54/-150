1fb7da3474157d6ebcbc5ab44c0b4ed1fa3198ec
nobita54 <a.k639583027@gmail.com>
2025-12-05 12:57:23 +0530

    Update ch.sh

--- Uninstall/ch.sh
+++ Uninstall/ch.sh
@@ -133,7 +133,7 @@ echo -e "${CYAN}─────────────────────�
 read -p "Select → " c
 
 case $c in
- 1) bash <(curl -s https://raw.githubusercontent.com/nobita54/-150/refs/heads/main/tools/vps.sh ;;
+ 1) bash <(curl -s https://raw.githubusercontent.com/nobita54/-150/refs/heads/main/tools/vps.sh) ;;
  2) panel_menu;;
  3) bash <(curl -s https://raw.githubusercontent.com/nobita54/-150/refs/heads/main/Uninstall/unwings.sh) ;;
  4) tools_menu;;
